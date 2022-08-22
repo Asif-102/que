8a64db08ffc8dccaa88a4cf3f72a41f947c0addc
Asif-102 <asif1zero2@gmail.com>
2022-08-22 18:48:57 +0600

    check MYQUEUE work ability

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "MYQUEUE.h"
 
 using namespace std;
 
 int main(void)
 {
-    
+    Queue q;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int chk;
+        cin >> chk;
+        q.push(chk);
+    }
+
+    while (!q.empty())
+        cout << q.pop() << " ";
+
+    if (!q.empty())
+        cout << q.Front() << endl;
+    if (!q.empty())
+        cout << q.Back() << endl;
 }
