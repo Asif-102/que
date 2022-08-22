01495c81a0a46bd1acd0406c8a340e027b6635da
Asif-102 <asif1zero2@gmail.com>
2022-08-22 20:37:35 +0600

    check with pair

--- main.cpp
+++ main.cpp
@@ -3,25 +3,59 @@
 
 using namespace std;
 
+typedef pair<int, int> mytype;
+
 int main(void)
 {
-    Queue<int> q;
+    // Queue<int> q;
+
+    // int n;
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int chk;
+    //     cin >> chk;
+    //     q.push(chk);
+    // }
+
+    // while (!q.empty())
+    //     cout << q.pop() << " ";
+
+    // if (!q.empty())
+    //     cout << q.Front() << endl;
+    // if (!q.empty())
+    //     cout << q.Back() << endl;
+
+    Queue<mytype> q;
 
     int n;
     cin >> n;
 
     for (int i = 0; i < n; i++)
     {
-        int chk;
-        cin >> chk;
-        q.push(chk);
+        int c1, c2;
+        cin >> c1 >> c2;
+        q.push(make_pair(c1, c2));
     }
 
     while (!q.empty())
-        cout << q.pop() << " ";
+    {
+        mytype chk;
+        chk = q.pop();
+        cout << chk.first << " | " << chk.second << endl;
+    }
 
     if (!q.empty())
-        cout << q.Front() << endl;
+    {
+        mytype chk;
+        chk = q.Front();
+        cout << chk.first << " | " << chk.second << endl;
+    }
     if (!q.empty())
-        cout << q.Back() << endl;
+    {
+        mytype chk;
+        chk = q.Back();
+        cout << chk.first << " | " << chk.second << endl;
+    }
 }
